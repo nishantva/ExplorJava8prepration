54181e0e15e37adf212918f0885dc996e4f14f8a
sniper <36824112+nishantva@users.noreply.github.com>
2025-12-29 14:42:00 +0530

    Update ChocolateDistributionProblem.java

--- array/ChocolateDistributionProblem.java
+++ array/ChocolateDistributionProblem.java
@@ -2,27 +2,22 @@ package array;
 
 import java.util.Arrays;
 
-public class ChocolateDistributionProblem {
-	
-	public static void main(String arg[]) {
-		int arr[]= {3, 4, 1, 9, 56, 7, 9, 12};
-		int n=arr.length;
-		
-		int m=5;
-		
-		int mindiff=Integer.MAX_VALUE;
-		
-		Arrays.sort(arr);
-		for(int i=0;i<n-m+1;i++) {
-			if((arr[i+m-1]  -arr[i])<mindiff) {
-				
-				mindiff=arr[i+m-1]-arr[i];
-				
-			}
-			
-		}
-		System.out.print(mindiff);
-		
-	}
+public class StockBuy {
 
+    public static void main(String[] args) {
+        int[] arr = {3, 4, 1, 9, 56, 7, 9, 12};
+        int m = 5;
+
+        Arrays.sort(arr);
+
+        int minDiff = Integer.MAX_VALUE;
+
+        for (int i = 0; i <= arr.length - m; i++) {
+            minDiff = Math.min(minDiff, arr[i + m - 1] - arr[i]);
+        }
+
+        System.out.println(minDiff);
+    }
 }
+
+
