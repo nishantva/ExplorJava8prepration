90fc2ed361d10f551b873cf274c515af48620a2d
sniper <36824112+nishantva@users.noreply.github.com>
2026-01-07 00:05:22 +0530

    Update SubarraywithZerosum.java

--- array/SubarraywithZerosum.java
+++ array/SubarraywithZerosum.java
@@ -2,37 +2,30 @@ package array;
 
 import java.util.HashMap;
 
-//good approch
 public class SubarraywithZerosum {
 
-	
-	
-	public static void main(String arg[]) {
-		//int arr[]= {15 ,-2 ,2 ,-8 ,1 ,7 ,10 ,23};
-		int arr[] ={ 15, -2, 2, -8, 1, 7, 10, 23 };
-		int n=arr.length;
-		int length=-1;
-		HashMap<Integer,Integer> map=new  HashMap<Integer,Integer>();
-		int count=0;
-		
-		
-		int i=-1;
-		int sum=0;
-		map.put(sum,i);
-		
-		for( i=0;i<n;i++) {
-			sum+=arr[i];
-			
-			if(map.containsKey(sum)) {
-				count+=map.get(sum);
-				
-				map.put(sum, map.get(sum)+1);
-			}
-			else {
-				map.put(sum, 1);
-			}
-		}
-		
-		System.out.println(count);
-	}
+    public static void main(String[] args) {
+
+        int arr[] = {15, -2, 2, -8, 1, 7, 10, 23};
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
+        int count = 0;
+
+        // Important initialization
+        map.put(0, 1);
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+
+            if (map.containsKey(sum)) {
+                count += map.get(sum);   // add frequency
+                map.put(sum, map.get(sum) + 1);
+            } else {
+                map.put(sum, 1);
+            }
+        }
+
+        System.out.println(count);
+    }
 }
