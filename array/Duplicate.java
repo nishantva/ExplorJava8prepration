886c1cb86a125c901a4bc5bb202a351bbaee86a6
sniper <36824112+nishantva@users.noreply.github.com>
2026-01-09 00:36:18 +0530

    Update Duplicate.java

--- array/Duplicate.java
+++ array/Duplicate.java
@@ -1,30 +1,19 @@
-package array;
+public class DuplicateFinder {
 
-public class Duplicate {
-	
-	
-	public static void main(Integer aarg[]) {
-		
-		int arr[]= {1,2,3,4,1,4,6,6};
-		
-		int n=arr.length;
-		
-		for(int i=0;i<n;i++) {
-			if(arr[Math.abs(arr[i])] < 0) {
-				System.out.println(Math.abs(arr[i]));
-				
-				
-			}
-			else 
-			{
-				arr[Math.abs(arr[i])]=  -1*arr[Math.abs(arr[i])];
-			}
-		//	System.out.print("hdjd");
-			
-		}
-		
-		
-		
-	}
+    public static void main(String[] args) {
 
+        int arr[] = {1,2,3,4,1,4,6,6};
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+
+            int index = Math.abs(arr[i]);
+
+            if (arr[index] < 0) {
+                System.out.println(index);
+            } else {
+                arr[index] = -arr[index];
+            }
+        }
+    }
 }
