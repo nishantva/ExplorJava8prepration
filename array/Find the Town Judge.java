e29318d1a5ad321babe5e0b85724f686f31364b5
sniper <36824112+nishantva@users.noreply.github.com>
2026-01-10 22:51:57 +0530

    Create  Find the Town Judge.java

---
+++ array/Find the Town Judge.java
@@ -0,0 +1,27 @@
+Input: n = 3, trust = [[1,3],[2,3]]
+Output: 3
+
+
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+
+        int indegree[] = new int[n + 1];
+        int outdegree[] = new int[n + 1];
+
+        for (int i = 0; i < trust.length; i++) {
+            int a = trust[i][0];
+            int b = trust[i][1];
+
+            outdegree[a]++;  // a trusts someone
+            indegree[b]++;   // b is trusted
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (indegree[i] == n - 1 && outdegree[i] == 0) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
