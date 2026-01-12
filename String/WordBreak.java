5661e61451816d827c475c68987ccf8063f4c649
sniper <36824112+nishantva@users.noreply.github.com>
2026-01-12 22:02:19 +0530

    WordBreak.java

--- String/WordBreak.java
+++ String/WordBreak.java
@@ -1,1 +1,28 @@
 
+Example 1:
+
+Input: s = "leetcode", wordDict = ["leet","code"]
+Output: true
+Explanation: Return true because "leetcode" can be segmented as "leet code".
+//--
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+
+        Set<String> dinct=new HashSet<String>(wordDict);
+        boolean [] dp =new boolean [s.length()+1];
+        dp[0]=true;
+
+        for(int i=1;i<=s.length();i++){
+
+            for(int j=0;j<i;j++){
+                if(dp[j] && dinct.contains(s.substring(j,i))){
+                    dp[i] =true;
+                    break;
+                }
+            }
+        }
+      return dp[s.length()];  
+        
+    }
+}
+  
