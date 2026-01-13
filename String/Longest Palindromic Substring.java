dd8fa974c4216d3e9a63932229dc0475c738108a
sniper <36824112+nishantva@users.noreply.github.com>
2026-01-13 12:22:54 +0530

    Update Longest Palindromic Substring.java

--- String/Longest Palindromic Substring.java
+++ String/Longest Palindromic Substring.java
@@ -6,25 +6,29 @@ Explanation: "aba" is also a valid answer.
 
   class Solution {
     public String longestPalindrome(String s) {
-     String longest="";
-     int n=s.length();
-     for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(ispalin(s,i,j)){
-                if(longest.length()<j-i+1){
-                    longest=s.substring(i,j+1);
+        String longest = "";
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {   // FIXED
+                if (ispalin(s, i, j)) {
+                    if (longest.length() < j - i + 1) {
+                        longest = s.substring(i, j + 1);
+                    }
                 }
             }
         }
-     }return longest;
-
+        return longest;
     }
-    public static boolean ispalin(String str,int left,int right){
-       while(left<right){
-        if(str.charAt(left)!=str.charAt(right)){
-            return false;
+
+    public static boolean ispalin(String str, int left, int right) {
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
         }
-        left++;right--;
-       }return true;
+        return true;
     }
 }
