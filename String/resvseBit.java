cca539ee6c9b591dbf713655dbdac25be6776835
sniper <36824112+nishantva@users.noreply.github.com>
2026-01-13 13:02:40 +0530

    Update resvseBit.java

--- String/resvseBit.java
+++ String/resvseBit.java
@@ -6,7 +6,7 @@ class Solution {
 
       int res=0;  //store result
 
-      for(int i=31;i>=0;i=i-1){   //Java int has 32 bits
+      for(int i=31;i>=0;i=i--){   //Java int has 32 bits
          int bit= n&1;  //Extracts the last (rightmost) bit of n
          res=res + (bit<<i);  //bit << i → move the bit to position i  niche se aage uss postion mai set kr dega//
          n= n >>1;  //Right-shift n by 1 // Removes the bit we just used   last bit remove hogyi
