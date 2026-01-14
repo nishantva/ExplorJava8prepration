92338e702772f0e494946dbfd6ea132df9caec12
sniper <36824112+nishantva@users.noreply.github.com>
2026-01-14 17:08:45 +0530

    Update TestCoustomException.java

--- Exception/TestCoustomException.java
+++ Exception/TestCoustomException.java
@@ -1,15 +1,13 @@
 package Exception;
 
-public class TestCoustomException {
-	
-	public static void main(String arg[]) {
-		
-		
-		try {
-			throw new CoustomException("this is the the coustom exception");
-		} catch(Exception e){
-			System.out.print(e);
-		}
-	}
 
+public class TestCustomException {
+    public static void main(String[] args) {
+        int age = 15;
+
+        if (age < 18) {
+            throw new InvalidAgeException("Age must be 18 or above");
+        }
+    }
 }
+
