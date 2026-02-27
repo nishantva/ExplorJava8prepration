e6a79666a94d67c5cd9d75a6c0cb421231306019
sniper <36824112+nishantva@users.noreply.github.com>
2026-02-27 14:16:11 +0530

    Update Find the Town Judge.java

--- array/Find the Town Judge.java
+++ array/Find the Town Judge.java
@@ -5,7 +5,7 @@ Output: 3
 class Solution {
     public int findJudge(int n, int[][] trust) {
 
-        int indegree[] = new int[n + 1];
+        int indegree[] = new int[n + 1]; //We create n + 1 because people are numbered from 1 to n
         int outdegree[] = new int[n + 1];
 
         for (int i = 0; i < trust.length; i++) {
